a39f14cfa8a3e58f715caae0562b6a81df969be6
J12934 <13718901+J12934@users.noreply.github.com>
2020-02-27 18:31:52 -0500

    Fix error

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
 
-try {
-  await exec.exec('npm run lint:fix');
-} catch (error) {
-  core.setFailed(
-    'The linters are failing. Your code is not formatted to standard and could not be automatically fixed.'
-  );
+async function main() {
+  try {
+    await exec.exec('npm run lint:fix');
+  } catch (error) {
+    core.setFailed(
+      'The linters are failing. Your code is not formatted to standard and could not be automatically fixed.'
+    );
+  }
 }
+
+main();
